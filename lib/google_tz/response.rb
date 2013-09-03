6f4f0fc05db2112a5629d16103d1af7a48ef1814
Daniel McGraw <daniel@trip30.com>
2013-09-02 19:38:45 -0600

    Playing with Rseponse

---
+++ lib/google_tz/response.rb
@@ -0,0 +1,37 @@
+require 'json'
+
+module GoogleTZAPI
+  class Response
+    def initialize(json)
+      @data = JSON.parse(json)
+    end
+
+    def success?
+      @data['status'].eql?('OK')
+    end
+
+    def dst_offset
+      @data.fetch('dstOffset', nil)
+    end
+
+    def raw_offset
+      @data.fetch('rawOffset', nil)
+    end
+
+    def status
+      @data.fetch('status')
+    end
+
+    def timezone_id
+      @data.fetch('timeZoneId', nil)
+    end
+
+    def timezone_name
+      @data.fetch('timeZoneName', nil)
+    end
+
+    def data
+      @data
+    end
+  end
+end
