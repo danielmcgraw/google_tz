cf2c5efea90fd1086b8f21470975207d96a41013
Daniel McGraw <daniel@trip30.com>
2013-09-02 19:59:32 -0600

    Update tests for Response object

--- spec/lib/google_tz_spec.rb
+++ spec/lib/google_tz_spec.rb
@@ -3,17 +3,25 @@ require 'spec_helper'
 describe 'GoogleTZ' do
   describe 'lookup' do
     it 'fetches timezone info for a given latitude, longitude' do
-      response = GoogleTZ.lookup(39.7392, -104.9847)
+      resp = GoogleTZ.lookup(39.7392, -104.9847)
 
-      response.should_not be_nil
-      response.should == "{\n   \"dstOffset\" : 3600,\n   \"rawOffset\" : -25200,\n   \"status\" : \"OK\",\n   \"timeZoneId\" : \"America/Denver\",\n   \"timeZoneName\" : \"Mountain Daylight Time\"\n}\n"
+      resp.success?.should == true
+      resp.status.should == 'OK'
+      resp.dst_offset.should == 3600
+      resp.raw_offset.should == -25200
+      resp.timezone_id.should == "America/Denver"
+      resp.timezone_name.should == "Mountain Daylight Time"
 	end
 
 	it 'fetches timezone info when given optional arguments' do
-	  response = GoogleTZ.lookup(39.7392, -104.9847, timestamp: 1377891332, sensor: false, language: "de")
+	  resp = GoogleTZ.lookup(39.7392, -104.9847, timestamp: 1377891332, sensor: false, language: "de")
 
-      response.should_not be_nil
-      response.should == "{\n   \"dstOffset\" : 3600,\n   \"rawOffset\" : -25200,\n   \"status\" : \"OK\",\n   \"timeZoneId\" : \"America/Denver\",\n   \"timeZoneName\" : \"Rocky Mountain Sommerzeit\"\n}\n"
+      resp.success?.should == true
+      resp.status.should == 'OK'
+      resp.dst_offset.should == 3600
+      resp.raw_offset.should == -25200
+      resp.timezone_id.should == "America/Denver"
+      resp.timezone_name.should == "Rocky Mountain Sommerzeit"
 	end
   end
 end
