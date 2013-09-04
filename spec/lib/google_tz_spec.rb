d6efe47927bebe809112af228479733e3e377a31
Daniel McGraw <daniel@trip30.com>
2013-09-03 22:04:09 -0600

    Stub lookup calls

--- spec/lib/google_tz_spec.rb
+++ spec/lib/google_tz_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe 'GoogleTZ' do
   describe 'lookup' do
     it 'fetches timezone info for a given latitude, longitude' do
+      GoogleTZ.should_receive(:lookup).and_return(GoogleTZAPI::Response.new("{\n   \"dstOffset\" : 3600,\n   \"rawOffset\" : -25200,\n   \"status\" : \"OK\",\n   \"timeZoneId\" : \"America/Denver\",\n   \"timeZoneName\" : \"Mountain Daylight Time\"\n}\n"))
       resp = GoogleTZ.lookup(39.7392, -104.9847)
-
+      
       resp.success?.should == true
       resp.status.should == 'OK'
       resp.dst_offset.should == 3600
@@ -14,19 +15,15 @@ describe 'GoogleTZ' do
 	end
 
 	it 'fetches timezone info when given optional arguments' do
+    GoogleTZ.should_receive(:lookup).and_return(GoogleTZAPI::Response.new("{\n   \"dstOffset\" : 3600,\n   \"rawOffset\" : -25200,\n   \"status\" : \"OK\",\n   \"timeZoneId\" : \"America/Denver\",\n   \"timeZoneName\" : \"Rocky Mountain Sommerzeit\"\n}\n"))
 	  resp = GoogleTZ.lookup(39.7392, -104.9847, timestamp: 1377891332, sensor: false, language: "de")
 
-      resp.success?.should == true
-      resp.status.should == 'OK'
-      resp.dst_offset.should == 3600
-      resp.raw_offset.should == -25200
-      resp.timezone_id.should == "America/Denver"
-      resp.timezone_name.should == "Rocky Mountain Sommerzeit"
+    resp.success?.should == true
+    resp.status.should == 'OK'
+    resp.dst_offset.should == 3600
+    resp.raw_offset.should == -25200
+    resp.timezone_id.should == "America/Denver"
+    resp.timezone_name.should == "Rocky Mountain Sommerzeit"
 	end
   end
 end
-
-
-
-"{\n   \"dstOffset\" : 3600,\n   \"rawOffset\" : -25200,\n   \"status\" : \"OK\",\n   \"timeZoneId\" : \"America/Denver\",\n   \"timeZoneName\" : \"Hora de verano de Montaña\"\n}\n"
-'{\n   \"dstOffset\" : 3600,\n   \"rawOffset\" : -25200,\n   \"status\" : \"OK\",\n   \"timeZoneId\" : \"America/Denver\",\n   \"timeZoneName\" : \"Hora de verano de Monta\\xC3\\xB1a\"\n}\n'
